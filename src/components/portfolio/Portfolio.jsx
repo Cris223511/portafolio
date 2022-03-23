1c33e5178d98ade54422c363036d1afa6c4c15ef
Cris223511 <cris_antonio2001@hotmail.com>
2022-03-23 09:58:30 -0500

    CV actualizado

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -32,6 +32,7 @@ const Portfolio = () => {
   return (
     <section id='portfolio'>
       <h5>¿Cuáles son mis proyectos?</h5>
+      <h5>Próximamente se irá actualizando esta sección</h5>
       <h2>Mis Proyectos</h2>
 
       <div className='container portfolio__container' id='contenedor1'>
