faae03ec58b8d561f136795765924520d5d97bb9
Cris223511 <cris_antonio2001@hotmail.com>
2026-01-14 01:41:36 -0500

    actualizo el primer section, quito la primera foto y arreeglo el diseño del primer section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/me.png'
 import HeaderSocial from './HeaderSocial'
 import { scrollToSection } from '../../utils/smoothScroll';
 
@@ -9,16 +8,17 @@ const Header = () => {
   return (
     <header>
       <div id='header' className="container header__container">
-        <h5 style={{ paddingTop: '30px' }}>Hola, mi nombre es</h5>
-        <h1>Christopher</h1>
-        <h5 className="text-light">Ingeniero de Sistemas de la Información</h5>
-        <CTA />
-        <HeaderSocial />
-
-        <div className="me">
-          <img unselectable='on' src={ME} alt="me" />
+        <div className="header__content">
+          <span className="header__greeting">👋 Hola, mi nombre es</span>
+          <h1 className="header__name">Christopher</h1>
+          <h2 className="header__title">Ingeniero de Sistemas de la Información</h2>
+          <p className="header__description">
+            Full Stack Developer con experiencia en desarrollo de sistemas empresariales
+            y aplicaciones web, con enfoque en backend y arquitecturas escalables.
+          </p>
+          <CTA />
         </div>
-
+        <HeaderSocial />
         <a href="#footer" className='scroll__down' onClick={(e) => { e.preventDefault(); scrollToSection('footer'); }}>Ir abajo</a>
       </div>
     </header>
